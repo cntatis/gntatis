b3788799e47667808cb24b85045578db5d8a6412
Charalampos Ntatis <cntatis@hotmail.com>
2021-01-02 16:12:32 +0200

    Added a loader

---
+++ main.js
@@ -0,0 +1,14 @@
+// Using Document.readyState to check the loader
+document.onreadystatechange = function() 
+{ 
+    if (document.readyState !== "complete") 
+    { 
+        document.querySelector( "body").style.visibility = "hidden"; 
+        document.querySelector( "#loader").style.visibility = "visible"; 
+    } 
+    else 
+    { 
+        document.querySelector( "body").style.visibility = "visible"; 
+        document.querySelector( "#loader").style.display = "none"; 
+    } 
+}; 
